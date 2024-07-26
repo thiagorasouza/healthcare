e617ba731ce4e7beffd9800dfd592d913729e297
Thiago Souza <thiagorasouza@gmail.com>
2024-07-25 19:42:03 -0500

    feat: wrap login error inside an alert shadcn component

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -2,7 +2,6 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -12,14 +11,18 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Input } from "@/components/ui/input";
 import { loginAdmin } from "./loginAdmin";
 import { loginSchema } from "./loginSchema";
 import { LoginData } from "./loginData";
 import { useState } from "react";
 import { unexpectedError } from "@/lib/results";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [message, setMessage] = useState("");
 
   const form = useForm<LoginData>({
@@ -33,8 +36,8 @@ export default function LoginPage() {
   async function onSubmit(loginData: LoginData) {
     setMessage("");
     try {
-      await new Promise((resolve) => setTimeout(resolve, 5000));
       const result = await loginAdmin(loginData);
+
       setMessage(result.message);
     } catch (error) {
       console.log("🚀 ~ error:", error);
@@ -49,7 +52,13 @@ export default function LoginPage() {
       <header>
         <h1 className="text-3xl font-semibold">Login</h1>
       </header>
-      {message && <p>{message}</p>}
+      {message && (
+        <Alert variant="destructive" className="text-sm">
+          <ExclamationTriangleIcon className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>{message}</AlertDescription>
+        </Alert>
+      )}
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <FormField
