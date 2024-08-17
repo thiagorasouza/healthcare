4fe8efa0a6a043dc15f461ebea79307caf3abe1f
Thiago Souza <thiagorasouza@gmail.com>
2024-08-17 14:38:03 -0500

    feat: show Add new slots button when there are no slots

--- components/slots/SlotsList.tsx
+++ components/slots/SlotsList.tsx
@@ -5,15 +5,23 @@ import SlotsCard from "@/components/slots/SlotsCard";
 import SlotsButtonList from "@/components/slots/SlotsButtonList";
 import { Spinner } from "@/components/ui/spinner";
 import { PatternDocumentListSchema, PatternDocumentSchema } from "@/lib/schemas/appwriteSchema";
+import { Button } from "@/components/ui/button";
 
 interface SlotsListProps {
   data: PatternDocumentListSchema | undefined;
   loading: boolean;
   onSlotClick: (pattern: PatternDocumentSchema) => void;
+  onCreateClick: () => void;
   className?: string;
 }
 
-export default function SlotsList({ data, loading, onSlotClick, className }: SlotsListProps) {
+export default function SlotsList({
+  data,
+  loading,
+  onSlotClick,
+  className,
+  onCreateClick,
+}: SlotsListProps) {
   if (loading) {
     return (
       <SlotsCard className={className}>
@@ -29,11 +37,18 @@ export default function SlotsList({ data, loading, onSlotClick, className }: Slo
   return (
     <SlotsCard className={className}>
       <div className="flex flex-col gap-6">
-        {data.total > 0
-          ? data.documents.map((pattern, index) => (
-              <SlotsButtonList key={index} data={pattern} onClick={() => onSlotClick(pattern)} />
-            ))
-          : "No slots yet."}
+        {data.total > 0 ? (
+          data.documents.map((pattern, index) => (
+            <SlotsButtonList key={index} data={pattern} onClick={() => onSlotClick(pattern)} />
+          ))
+        ) : (
+          <>
+            <p>No slots yet, start by clicking below</p>
+            <Button className="w-fit" onClick={onCreateClick}>
+              Add new slots
+            </Button>
+          </>
+        )}
       </div>
     </SlotsCard>
   );
