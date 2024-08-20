c772472a378c252ed44dfd8b74d3c5382ba8bcea
Thiago Souza <thiagorasouza@gmail.com>
2024-08-20 07:54:41 -0500

    test: add createCollections script

---
+++ lib/scripts/createCollections.ts
@@ -0,0 +1,76 @@
+import { databases } from "@/lib/appwrite/adminClient";
+import { genderTypes, idTypes } from "@/lib/constants";
+import { env } from "@/lib/env";
+
+const dbId = env.databaseId;
+
+async function createDoctorCollection() {
+  const colId = env.doctorsCollectionId;
+
+  try {
+    await databases.deleteCollection(dbId, colId);
+    await databases.createCollection(dbId, colId, "doctors", undefined, true);
+    await databases.createStringAttribute(dbId, colId, "name", 100, true);
+    await databases.createEmailAttribute(dbId, colId, "email", true);
+    await databases.createStringAttribute(dbId, colId, "phone", 100, true);
+    await databases.createStringAttribute(dbId, colId, "specialty", 100, true);
+    await databases.createStringAttribute(dbId, colId, "bio", 500, true);
+    await databases.createStringAttribute(dbId, colId, "pictureId", 100, true);
+
+    console.log("Doctors collection created");
+  } catch (error) {
+    console.log("🚀 ~ error:", error);
+  }
+}
+
+async function createPatientCollection() {
+  const idTypesArr = idTypes.map((idType) => idType.value);
+  const genderTypesArr = genderTypes.map((genderTypes) => genderTypes.value);
+  const colId = env.patientsCollectionId;
+
+  try {
+    await databases.deleteCollection(dbId, colId);
+    await databases.createCollection(dbId, colId, "patients", undefined, true);
+    await databases.createStringAttribute(dbId, colId, "name", 100, true);
+    await databases.createEmailAttribute(dbId, colId, "email", true);
+    await databases.createStringAttribute(dbId, colId, "phone", 100, true);
+    await databases.createDatetimeAttribute(dbId, colId, "birthdate", true);
+    await databases.createEnumAttribute(dbId, colId, "gender", genderTypesArr, true);
+    await databases.createStringAttribute(dbId, colId, "address", 200, true);
+    await databases.createStringAttribute(dbId, colId, "insuranceProvider", 100, true);
+    await databases.createStringAttribute(dbId, colId, "insuranceNumber", 100, true);
+    await databases.createEnumAttribute(dbId, colId, "identificationType", idTypesArr, true);
+    await databases.createStringAttribute(dbId, colId, "identificationNumber", 100, true);
+    await databases.createStringAttribute(dbId, colId, "identificationId", 100, true);
+    await databases.createBooleanAttribute(dbId, colId, "usageConsent", true);
+    await databases.createBooleanAttribute(dbId, colId, "privacyConsent", true);
+
+    console.log("Patients collection created");
+  } catch (error) {
+    console.log("🚀 ~ error:", error);
+  }
+}
+
+async function createPatternsCollection() {
+  const colId = env.patternsCollectionId;
+
+  try {
+    await databases.deleteCollection(dbId, colId);
+    await databases.createCollection(dbId, colId, "patterns", undefined, true);
+    await databases.createDatetimeAttribute(dbId, colId, "startDate", true);
+    await databases.createDatetimeAttribute(dbId, colId, "endDate", true);
+    await databases.createDatetimeAttribute(dbId, colId, "startTime", true);
+    await databases.createDatetimeAttribute(dbId, colId, "endTime", true);
+    await databases.createIntegerAttribute(dbId, colId, "duration", true);
+    await databases.createBooleanAttribute(dbId, colId, "recurring", true);
+    await databases.createStringAttribute(dbId, colId, "weekdays", 100, false, undefined, true);
+
+    console.log("Patterns collection created");
+  } catch (error) {
+    console.log("🚀 ~ error:", error);
+  }
+}
+
+createDoctorCollection();
+createPatientCollection();
+createPatternsCollection();
