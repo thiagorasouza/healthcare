aaa9fe78ff319f1e7e21dab0850b8183a94e32c5
Thiago Souza <thiagorasouza@gmail.com>
2024-09-10 13:23:49 -0500

    feat: ensure that getSlotsByDate works for a single date and a recurring pattern combined

--- __tests__/getSlotsByDate.test.ts
+++ __tests__/getSlotsByDate.test.ts
@@ -5,7 +5,7 @@ import { getSlotsByDate } from "@/lib/processing/getSlotsByDate";
 describe("getSlotsByDate Test Suite", () => {
   it("should return slots for a single date", () => {
     const singleDateMock = mockSingleDate();
-    const result = getSlotsByDate(singleDateMock);
+    const result = getSlotsByDate([singleDateMock]);
     expect(result).toStrictEqual({
       "2024-01-01T05:00:00.000Z": [
         ["10:00", "10:30"],
@@ -16,13 +16,35 @@ describe("getSlotsByDate Test Suite", () => {
 
   it("should return slots for a recurring pattern", () => {
     const recurringPatternMock = mockRecurringPattern();
-    const result = getSlotsByDate(recurringPatternMock);
+    const result = getSlotsByDate([recurringPatternMock]);
     expect(result).toStrictEqual({
       "2024-01-01T05:00:00.000Z": [
         ["08:00", "08:30"],
         ["08:30", "09:00"],
         ["09:00", "09:30"],
         ["09:30", "10:00"],
+      ],
+      "2024-01-02T05:00:00.000Z": [
+        ["08:00", "08:30"],
+        ["08:30", "09:00"],
+        ["09:00", "09:30"],
+        ["09:30", "10:00"],
+      ],
+    });
+  });
+
+  it("should return slots for a single date and a recurring pattern", () => {
+    const singleDateMock = mockSingleDate();
+    const recurringPatternMock = mockRecurringPattern();
+    const result = getSlotsByDate([singleDateMock, recurringPatternMock]);
+    expect(result).toStrictEqual({
+      "2024-01-01T05:00:00.000Z": [
+        ["08:00", "08:30"],
+        ["08:30", "09:00"],
+        ["09:00", "09:30"],
+        ["09:30", "10:00"],
+        ["10:00", "10:30"],
+        ["10:30", "11:00"],
       ],
       "2024-01-02T05:00:00.000Z": [
         ["08:00", "08:30"],
--- lib/processing/getSlotsByDate.ts
+++ lib/processing/getSlotsByDate.ts
@@ -1,18 +1,6 @@
-import { Pattern, PatternData, Weekday } from "@/lib/schemas/patternsSchema";
-import {
-  getFirstWeekdayAfter,
-  getHourStrFromDate as getTimeStrFromDate,
-  transposeTime,
-} from "@/lib/utils";
-import {
-  addWeeks,
-  differenceInWeeks,
-  format,
-  isAfter,
-  isBefore,
-  isSameDay,
-  startOfDay,
-} from "date-fns";
+import { Pattern, Weekday } from "@/lib/schemas/patternsSchema";
+import { getFirstWeekdayAfter, getHourStrFromDate as getTimeStrFromDate } from "@/lib/utils";
+import { addWeeks, differenceInWeeks, isAfter, isBefore, isSameDay, startOfDay } from "date-fns";
 
 /**
  * {
@@ -31,69 +19,68 @@ interface LimitOptions {
   exactDate?: Date;
 }
 
-export function getSlotsByDate(data: Pattern, limit?: LimitOptions): SlotsByDate {
-  const { startDate, endDate, startTime, endTime, duration, weekdays, recurring } = data;
-  // console.log("🚀 ~ startDate:", startDate);
-  // console.log("🚀 ~ endDate:", endDate);
-  // console.log("🚀 ~ weekdays:", weekdays);
-  let weekdaysToLoop = weekdays;
-  if (limit?.weekdays) {
-    weekdaysToLoop = weekdays.filter((weekday) => limit.weekdays!.includes(weekday as Weekday));
-  }
-
-  const startTimeMs = startTime.getTime();
-  const endTimeMs = endTime.getTime();
-  const diffMs = endTimeMs - startTimeMs;
-  const durationMs = duration * 60 * 1000;
-  const slotsNum = diffMs / durationMs;
-
-  const slots = [];
-  // console.log("🚀 ~ slotsToTranspose:", slotsToTranspose);
-  for (let i = 0; i < slotsNum; i++) {
-    const startMs = startTimeMs + durationMs * i;
-    const endMs = startMs + durationMs;
-    const startStr = getTimeStrFromDate(new Date(startMs));
-    const endStr = getTimeStrFromDate(new Date(endMs));
-    slots.push([startStr, endStr]);
-  }
-
-  if (!recurring) {
-    const dateStr = getNormalizedDateStr(startDate);
-    return {
-      [dateStr]: slots,
-    };
-  }
-
+export function getSlotsByDate(patterns: Pattern[], limit?: LimitOptions): SlotsByDate {
   const result = {} as SlotsByDate;
-  const weeksNum = differenceInWeeks(endDate, startDate) + 1;
-  // console.log("🚀 ~ weeksNum:", weeksNum);
-  weeksLoop: for (let i = 0; i < weeksNum; i++) {
-    for (const weekday of weekdaysToLoop) {
-      const start = addWeeks(startDate, i);
-      const date = getFirstWeekdayAfter(start, weekday);
+  for (const pattern of patterns) {
+    const { startDate, endDate, startTime, endTime, duration, weekdays, recurring } = pattern;
 
-      if (isAfter(date, endDate)) {
-        break weeksLoop;
+    let weekdaysToLoop = weekdays;
+    if (limit?.weekdays) {
+      weekdaysToLoop = weekdays.filter((weekday) => limit.weekdays!.includes(weekday as Weekday));
+    }
+
+    const startTimeMs = startTime.getTime();
+    const endTimeMs = endTime.getTime();
+    const diffMs = endTimeMs - startTimeMs;
+    const durationMs = duration * 60 * 1000;
+    const slotsNum = diffMs / durationMs;
+
+    const slots = [];
+    for (let i = 0; i < slotsNum; i++) {
+      const startMs = startTimeMs + durationMs * i;
+      const endMs = startMs + durationMs;
+      const startStr = getTimeStrFromDate(new Date(startMs));
+      const endStr = getTimeStrFromDate(new Date(endMs));
+      slots.push([startStr, endStr]);
+    }
+
+    if (!recurring) {
+      const dateStr = getNormalizedDateStr(startDate);
+      result[dateStr] = Array.isArray(result[dateStr]) ? result[dateStr].concat(slots) : slots;
+      continue;
+    }
+
+    const weeksNum = differenceInWeeks(endDate, startDate) + 1;
+    weeksLoop: for (let i = 0; i < weeksNum; i++) {
+      for (const weekday of weekdaysToLoop) {
+        const start = addWeeks(startDate, i);
+        const date = getFirstWeekdayAfter(start, weekday);
+
+        if (isAfter(date, endDate)) {
+          break weeksLoop;
+        }
+
+        if (limit?.exactDate && !isSameDay(date, limit.exactDate)) {
+          break;
+        }
+
+        if (limit?.start && isBefore(date, limit.start)) {
+          break;
+        }
+
+        if (limit?.end && isAfter(date, limit.end)) {
+          break weeksLoop;
+        }
+
+        const dateStr = getNormalizedDateStr(date);
+        result[dateStr] = Array.isArray(result[dateStr]) ? result[dateStr].concat(slots) : slots;
       }
-
-      if (limit?.exactDate && !isSameDay(date, limit.exactDate)) {
-        break;
-      }
-
-      if (limit?.start && isBefore(date, limit.start)) {
-        break;
-      }
-
-      if (limit?.end && isAfter(date, limit.end)) {
-        break weeksLoop;
-      }
-
-      const dateStr = getNormalizedDateStr(date);
-
-      result[dateStr] = slots;
     }
   }
 
+  for (const dateStr in result) {
+    result[dateStr].sort((a, b) => (a[0] < b[0] ? -1 : 1));
+  }
   return result;
 }
 
