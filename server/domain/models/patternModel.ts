3faa71ee520fc2110484602aac1a4d6c32c144e6
Thiago Souza <thiagorasouza@gmail.com>
2024-09-12 17:34:18 -0500

    feat: add PatternModel class

---
+++ server/domain/models/patternModel.ts
@@ -0,0 +1,13 @@
+export type Weekday = "mon" | "tue" | "wed" | "thu" | "fri" | "sat" | "sun";
+
+export interface PatternModel {
+  id?: string;
+  startDate: Date;
+  endDate: Date;
+  startTime: Date;
+  endTime: Date;
+  duration: number;
+  recurring: boolean;
+  weekdays: Weekday[];
+  doctorId: string;
+}
