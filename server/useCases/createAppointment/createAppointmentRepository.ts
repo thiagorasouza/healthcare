7deacff5362d776466c8359d130ad4b07eea3030
Thiago Souza <thiagorasouza@gmail.com>
2024-09-12 17:55:26 -0500

    feat: add getPatternsByDoctorId to CreateAppointmentRepository

--- server/useCases/createAppointment/createAppointmentRepository.ts
+++ server/useCases/createAppointment/createAppointmentRepository.ts
@@ -1,9 +1,12 @@
 import { DoctorNotFoundFailure } from "@/server/shared/failures/doctorNotFoundFailure";
 import { PatientNotFoundFailure } from "@/server/shared/failures/patientNotFoundFailure";
+import { PatternNotFoundFailure } from "@/server/shared/failures/patternNotFoundFailure";
 import { DoctorFoundSuccess } from "@/server/shared/successes/doctorFoundSuccess";
 import { PatientFoundSuccess } from "@/server/shared/successes/patientFoundSuccess";
+import { PatternsFoundSuccess } from "@/server/shared/successes/patternsFoundSuccess";
 
 export interface CreateAppointmentRepository {
   getDoctorById(doctorId: string): Promise<DoctorFoundSuccess | DoctorNotFoundFailure>;
   getPatientById(patientId: string): Promise<PatientFoundSuccess | PatientNotFoundFailure>;
+  getPatternsByDoctorId(doctorId: string): Promise<PatternsFoundSuccess | PatternNotFoundFailure>;
 }
