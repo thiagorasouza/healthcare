385be47eefb8ac369fb0019c11e9d5cb61077144
Thiago Souza <thiagorasouza@gmail.com>
2024-09-12 17:34:40 -0500

    feat: add weekdays list to constants

--- server/config/constants.ts
+++ server/config/constants.ts
@@ -1,6 +1,8 @@
 import { Gender, IdentificationType } from "@/server/domain/models/patientModel";
+import { Weekday } from "@/server/domain/models/patternModel";
 
 export const genders: Gender[] = ["male", "female", "other"];
+export const weekdays: Weekday[] = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 export const identificationTypes: IdentificationType[] = [
   "passport",
   "identityCard",
