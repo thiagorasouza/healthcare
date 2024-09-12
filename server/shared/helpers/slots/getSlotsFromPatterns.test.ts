6db70f20007b7f095b8a247f6c02de07417add55
Thiago Souza <thiagorasouza@gmail.com>
2024-09-12 18:24:36 -0500

    fix: fix getSlotsFromPatterns test suite to reference the right function

--- server/shared/helpers/slots/getSlotsFromPatterns.test.ts
+++ server/shared/helpers/slots/getSlotsFromPatterns.test.ts
@@ -1,11 +1,33 @@
 import { expect, jest, test } from "@jest/globals";
-import { mockRecurringPattern, mockSingleDate } from "@/__tests__/mocks/pattern.mock";
-import { getSlots } from "@/lib/processing/getSlots";
+import { Weekday } from "@/server/domain/models/patternModel";
+import { getSlotsFromPatterns } from "@/server/shared/helpers/slots/getSlotsFromPatterns";
+
+export const mockSingleDate = (startTime = 10, endTime = 11) => ({
+  startDate: new Date("2024-01-01T05:00:00.000Z"),
+  endDate: new Date("2024-01-01T05:00:00.000Z"),
+  startTime: new Date(new Date().setHours(startTime, 0, 0, 0)),
+  endTime: new Date(new Date().setHours(endTime, 0, 0, 0)),
+  duration: 30,
+  recurring: false,
+  weekdays: [],
+  doctorId: "any_id",
+});
+
+export const mockRecurringPattern = (startTime = 8, endTime = 10) => ({
+  startDate: new Date("2024-01-01T05:00:00.000Z"),
+  endDate: new Date("2024-01-02T05:00:00.000Z"),
+  startTime: new Date(new Date().setHours(startTime, 0, 0, 0)),
+  endTime: new Date(new Date().setHours(endTime, 0, 0, 0)),
+  duration: 30,
+  recurring: true,
+  weekdays: ["mon", "tue"] as Weekday[],
+  doctorId: "any_id",
+});
 
 describe("getSlotsFromPatterns Test Suite", () => {
   it("should return slots for a single date", () => {
     const singleDateMock = mockSingleDate(10, 11);
-    const result = getSlots([singleDateMock]);
+    const result = getSlotsFromPatterns([singleDateMock]);
     expect(result).toStrictEqual({
       "2024-01-01T05:00:00.000Z": [
         ["10:00", "10:30"],
@@ -16,7 +38,7 @@ describe("getSlotsFromPatterns Test Suite", () => {
 
   it("should return slots for a recurring pattern", () => {
     const recurringPatternMock = mockRecurringPattern(8, 10);
-    const result = getSlots([recurringPatternMock]);
+    const result = getSlotsFromPatterns([recurringPatternMock]);
     expect(result).toStrictEqual({
       "2024-01-01T05:00:00.000Z": [
         ["08:00", "08:30"],
@@ -36,7 +58,7 @@ describe("getSlotsFromPatterns Test Suite", () => {
   it("should return slots for a single date and a recurring pattern", () => {
     const singleDateMock = mockSingleDate(10, 11);
     const recurringPatternMock = mockRecurringPattern(8, 10);
-    const result = getSlots([singleDateMock, recurringPatternMock]);
+    const result = getSlotsFromPatterns([singleDateMock, recurringPatternMock]);
     expect(result).toStrictEqual({
       "2024-01-01T05:00:00.000Z": [
         ["08:00", "08:30"],
@@ -58,7 +80,7 @@ describe("getSlotsFromPatterns Test Suite", () => {
   it("should return slots for two single dates", () => {
     const singleDateMock1 = mockSingleDate(10, 11);
     const singleDateMock2 = mockSingleDate(11, 12);
-    const result = getSlots([singleDateMock1, singleDateMock2]);
+    const result = getSlotsFromPatterns([singleDateMock1, singleDateMock2]);
     expect(result).toStrictEqual({
       "2024-01-01T05:00:00.000Z": [
         ["10:00", "10:30"],
@@ -72,7 +94,7 @@ describe("getSlotsFromPatterns Test Suite", () => {
   it("should return slots for two recurring patterns", () => {
     const recurringPatternMock1 = mockRecurringPattern(10, 11);
     const recurringPatternMock2 = mockRecurringPattern(11, 12);
-    const result = getSlots([recurringPatternMock1, recurringPatternMock2]);
+    const result = getSlotsFromPatterns([recurringPatternMock1, recurringPatternMock2]);
     expect(result).toStrictEqual({
       "2024-01-01T05:00:00.000Z": [
         ["10:00", "10:30"],
