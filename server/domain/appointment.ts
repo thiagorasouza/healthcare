37d2c9e1b7dee6c6cffd4446dda45821146b4321
Thiago Souza <thiagorasouza@gmail.com>
2024-09-11 20:33:32 -0500

    refactor: refactor Appointment entity

--- server/domain/appointment.ts
+++ server/domain/appointment.ts
@@ -2,13 +2,11 @@ import { AppointmentModel } from "@/server/domain/models/appointmentModel";
 import { LogicFailure } from "@/server/shared/failures/logicFailure";
 import { AppointmentLogicSuccess } from "@/server/shared/successes/appointmentLogicSuccess";
 
-type EntityCreationResult = AppointmentLogicSuccess | LogicFailure;
-
 export class Appointment {
   private constructor(public readonly model: AppointmentModel) {}
 
   // New appointments
-  public static create(data: AppointmentModel): EntityCreationResult {
+  public static create(data: AppointmentModel): AppointmentLogicSuccess | LogicFailure {
     const now = new Date();
     if (data.startTime < now) {
       return new LogicFailure(["startTime"]);
