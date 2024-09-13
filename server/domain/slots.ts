fd901a2f42df914633eb5fe88677a6ee2e4155da
Thiago Souza <thiagorasouza@gmail.com>
2024-09-12 20:16:39 -0500

    refactor: refactor Slots entity

--- server/domain/slots.ts
+++ server/domain/slots.ts
@@ -1,9 +1,18 @@
+import { weekdays } from "@/server/config/constants";
 import { PatternModel, Weekday } from "@/server/domain/models/patternModel";
 import { SlotsModel } from "@/server/domain/models/slotsModel";
 import { getFirstWeekdayAfter, getHourStrFromDate } from "@/server/shared/helpers/dateHelpers";
-import { addWeeks, differenceInWeeks, isAfter, isBefore, isSameDay, startOfDay } from "date-fns";
+import {
+  addWeeks,
+  differenceInWeeks,
+  getDay,
+  isAfter,
+  isBefore,
+  isSameDay,
+  startOfDay,
+} from "date-fns";
 
-interface Options {
+interface Limits {
   start?: Date;
   end?: Date;
   weekdays?: Weekday[];
@@ -13,33 +22,29 @@ interface Options {
 export class Slots {
   private constructor(public readonly model: SlotsModel) {}
 
-  public static create(patterns: PatternModel[], options?: Options) {
+  public static create(patterns: PatternModel[], limits?: Limits) {
     const result = new Map() as SlotsModel;
+
+    const pushSlots = (dateStr: string, slots: string[][]) =>
+      result.set(dateStr, result.has(dateStr) ? result.get(dateStr)!.concat(slots) : slots);
 
     for (const pattern of patterns) {
       const { startDate, endDate, startTime, endTime, duration, weekdays, recurring } = pattern;
 
-      let weekdaysToLoop = weekdays;
-      if (options?.weekdays) {
-        weekdaysToLoop = weekdays.filter((weekday) =>
-          options.weekdays!.includes(weekday as Weekday),
-        );
-      }
-
       const slots = Slots.getDailySlots(startTime, endTime, duration);
 
       if (!recurring) {
-        if (
-          (options?.exactDate && !isSameDay(startDate, options.exactDate)) ||
-          (options?.start && isBefore(startDate, options.start)) ||
-          (options?.end && isAfter(startDate, options.end))
-        ) {
-          continue;
+        if (!limits || !Slots.isDateOutOfLimits(startDate, limits)) {
+          pushSlots(Slots.toNormalizedDateStr(startDate), slots);
         }
-
-        const dateStr = startOfDay(startDate).toISOString();
-        result.set(dateStr, result.has(dateStr) ? result.get(dateStr)!.concat(slots) : slots);
         continue;
+      }
+
+      let weekdaysToLoop = weekdays;
+      if (limits?.weekdays) {
+        weekdaysToLoop = weekdays.filter((weekday) =>
+          limits.weekdays!.includes(weekday as Weekday),
+        );
       }
 
       const weeksNum = differenceInWeeks(endDate, startDate) + 1;
@@ -48,24 +53,18 @@ export class Slots {
           const start = addWeeks(startDate, i);
           const date = getFirstWeekdayAfter(start, weekday);
 
-          if (isAfter(date, endDate)) {
+          if (isAfter(date, endDate) || (limits?.end && isAfter(date, limits.end))) {
             break weeksLoop;
           }
 
-          if (options?.exactDate && !isSameDay(date, options.exactDate)) {
+          if (
+            (limits?.exactDate && !isSameDay(date, limits.exactDate)) ||
+            (limits?.start && isBefore(date, limits.start))
+          ) {
             break;
           }
 
-          if (options?.start && isBefore(date, options.start)) {
-            break;
-          }
-
-          if (options?.end && isAfter(date, options.end)) {
-            break weeksLoop;
-          }
-
-          const dateStr = startOfDay(date).toISOString();
-          result.set(dateStr, result.has(dateStr) ? result.get(dateStr)!.concat(slots) : slots);
+          pushSlots(Slots.toNormalizedDateStr(date), slots);
         }
       }
     }
@@ -93,5 +92,18 @@ export class Slots {
     }
 
     return slots;
+  }
+
+  public static isDateOutOfLimits(date: Date, limits: Limits) {
+    return (
+      (limits?.exactDate && !isSameDay(date, limits.exactDate)) ||
+      (limits?.start && isBefore(date, limits.start)) ||
+      (limits?.end && isAfter(date, limits.end)) ||
+      (limits?.weekdays && !weekdays.includes(weekdays[getDay(date)]))
+    );
+  }
+
+  public static toNormalizedDateStr(date: Date) {
+    return startOfDay(date).toISOString();
   }
 }
