a735558de9bccf69016be23fae17ab0526db2885
Thiago Souza <thiagorasouza@gmail.com>
2024-09-14 19:43:08 -0500

    feat: add appwrite helpers

---
+++ server/frameworks/appwrite/appwriteHelpers.ts
@@ -0,0 +1,9 @@
+import { AppwriteException, Models } from "node-appwrite";
+
+export function isAppwriteException(error: any): error is AppwriteException {
+  return (
+    typeof error === "object" && error !== null && error.constructor.name === "AppwriteException"
+  );
+}
+
+export type Appwritify<T> = Omit<T, "id"> & Models.Document;
