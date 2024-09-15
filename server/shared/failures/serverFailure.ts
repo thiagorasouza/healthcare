a9e4003395f2d0ade960b38b298be0951ef088bc
Thiago Souza <thiagorasouza@gmail.com>
2024-09-14 19:42:34 -0500

    refactor: refactor ServerFailure to accept a parameter of type any

--- server/shared/failures/serverFailure.ts
+++ server/shared/failures/serverFailure.ts
@@ -1,7 +1,7 @@
 import { Failure } from "@/server/core/failure";
 
-export class ServerFailure extends Failure<string> {
-  constructor(msg: string) {
+export class ServerFailure extends Failure<any> {
+  constructor(msg: any) {
     super(msg);
   }
 }
