4d803d131d96054c6873c2a202fbcc769d596b3a
Thiago Souza <thiagorasouza@gmail.com>
2024-09-30 14:51:54 -0500

    refactor: make getPatientById use generic getDocumentById from AppwriteRepository

--- server/frameworks/appwrite/appwritePatientsRepository.ts
+++ server/frameworks/appwrite/appwritePatientsRepository.ts
@@ -1,47 +1,22 @@
-import { DoctorModel } from "@/server/domain/models/doctorModel";
 import { PatientModel } from "@/server/domain/models/patientModel";
 import { Appwritify } from "@/server/frameworks/appwrite/appwriteHelpers";
 import { AppwriteRepository } from "@/server/frameworks/appwrite/appwriteRepository";
 import { PatientsRepository } from "@/server/repositories/patientsRepository";
-import { PatientNotFoundFailure } from "@/server/shared/failures";
-import { ServerFailure } from "@/server/shared/failures/serverFailure";
-import { PatientFoundSuccess } from "@/server/shared/successes";
 import { Databases } from "node-appwrite";
 
-export class AppwritePatientsRepository extends AppwriteRepository implements PatientsRepository {
+export class AppwritePatientsRepository
+  extends AppwriteRepository<PatientModel>
+  implements PatientsRepository
+{
   constructor(databases: Databases, databaseId: string, collectionId: string) {
     super(databases, databaseId, collectionId);
   }
 
-  async getPatientById(patientId: string): Promise<PatientFoundSuccess | PatientNotFoundFailure> {
-    try {
-      const result = (await this.getDocument(patientId)) as Appwritify<PatientModel>;
-      if (!result) {
-        return new PatientNotFoundFailure(patientId);
-      }
-
-      return new PatientFoundSuccess(this.map(result));
-    } catch (error) {
-      return new ServerFailure(error);
-    }
+  async getPatientById(patientId: string) {
+    return await this.getDocumentById(patientId);
   }
 
-  // public async getPatientById(
-  //   patientId: string,
-  // ): Promise<DoctorFoundSuccess | DoctorNotFoundFailure> {
-  //   try {
-  //     const result = (await this.getDocument(doctorId)) as Appwritify<DoctorModel>;
-  //     if (!result) {
-  //       return new DoctorNotFoundFailure(doctorId);
-  //     }
-
-  //     return new DoctorFoundSuccess(this.map(result));
-  //   } catch (error) {
-  //     return new ServerFailure(error);
-  //   }
-  // }
-
-  private map(data: Appwritify<PatientModel>): PatientModel {
+  public map(data: Appwritify<PatientModel>): PatientModel {
     return {
       id: data.$id,
       name: data.name,
