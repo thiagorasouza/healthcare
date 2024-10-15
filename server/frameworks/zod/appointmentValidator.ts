61fb708f287f014ba1070f72527c1d6e4fd9164c
Thiago Souza <thiagorasouza@gmail.com>
2024-10-15 11:09:03 -0500

    feat: add appointment validator

---
+++ server/frameworks/zod/appointmentValidator.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+import { GenericValidator } from "@/server/frameworks/zod/genericValidator";
+import { AppointmentModel } from "@/server/domain/models/appointmentModel";
+
+export const appointmentsSchema = z.object({
+  doctorId: z.string(),
+  patientId: z.string(),
+  startTime: z.coerce.date(),
+  duration: z.coerce.number(),
+});
+
+export const appointmentValidator = new GenericValidator<AppointmentModel>(appointmentsSchema);
