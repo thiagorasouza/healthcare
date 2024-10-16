8023fd7c7e14b7607cb5b9dd67ac739c60360f23
Thiago Souza <thiagorasouza@gmail.com>
2024-10-16 09:06:07 -0500

    fix: fix getFileMetadataServert to be a server action

--- lib/actions/getFileMetadataServer.ts
+++ lib/actions/getFileMetadataServer.ts
@@ -1,7 +1,11 @@
+"use server";
+
 import { storage } from "@/lib/appwrite/adminClient";
 import { success, unexpectedError } from "@/lib/results";
 
 export async function getFileMetadataServer(bucketId: string, fileId: string) {
+  // console.log("🚀 ~ fileId:", fileId);
+  // console.log("🚀 ~ bucketId:", bucketId);
   try {
     const result = await storage.getFile(bucketId, fileId);
     return success(result);
