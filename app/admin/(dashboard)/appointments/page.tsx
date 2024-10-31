3a240e3cfe5f742a316d317361548c33d0a9e6df
Thiago Souza <thiagorasouza@gmail.com>
2024-10-31 16:10:49 +0000

    refactor: remove comments

--- app/admin/(dashboard)/appointments/page.tsx
+++ app/admin/(dashboard)/appointments/page.tsx
@@ -5,9 +5,7 @@ import { listAppointments } from "@/server/actions/listAppointments";
 
 export default async function AppointmentsPage() {
   const appointmentsResult = await listAppointments();
-  console.log("🚀 ~ appointmentsResult:", appointmentsResult);
   const appointments = appointmentsResult.ok ? appointmentsResult.value : "error";
-  // const appointments = "error";
 
   return <AppointmentsTable appointments={appointments} />;
 }
