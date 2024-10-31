99bcc77115c4a4ccc58e135f54b3ffa082938e38
Thiago Souza <thiagorasouza@gmail.com>
2024-10-31 15:55:53 +0000

    refactor: remove unused files

--- server/actions/listAppointments.bypass.ts
+++
@@ -1,6 +0,0 @@
-import { AppointmentsRepository } from "@/server/frameworks/appwrite/appointmentsRepository";
-
-export const listAppointments = async () => {
-  const repository = new AppointmentsRepository();
-  return await repository.list();
-};
