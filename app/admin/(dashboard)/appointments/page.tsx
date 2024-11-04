8a5b2414a88963099e37a255708502f78e0d560f
Thiago Souza <thiagorasouza@gmail.com>
2024-11-04 18:09:04 +0000

    feat: add create button to appointments page

--- app/admin/(dashboard)/appointments/page.tsx
+++ app/admin/(dashboard)/appointments/page.tsx
@@ -1,11 +1,29 @@
 "use server";
 
+import AdminBreadcrumb from "@/components/admin/AdminBreadcrumb";
 import AppointmentsTable from "@/components/appointments/AppointmentsTable";
+import DefaultCard from "@/components/shared/DefaultCard";
+import { Button } from "@/components/ui/button";
 import { listAppointments } from "@/server/actions/listAppointments";
+import { PlusCircle } from "lucide-react";
+import Link from "next/link";
 
 export default async function AppointmentsPage() {
   const appointmentsResult = await listAppointments();
   const appointments = appointmentsResult.ok ? appointmentsResult.value : "error";
 
-  return <AppointmentsTable appointments={appointments} />;
+  return (
+    <div className="space-y-6">
+      <div className="flex items-center justify-between">
+        <AdminBreadcrumb />
+        <Button size="sm" className="ml-auto h-8 w-fit">
+          <Link href="/admin/appointments/create" className="flex items-center gap-2">
+            <PlusCircle className="h-3.5 w-3.5" />
+            <span className="sm:whitespace-nowrap">Add Appointment</span>
+          </Link>
+        </Button>
+      </div>
+      <AppointmentsTable appointments={appointments} />
+    </div>
+  );
 }
